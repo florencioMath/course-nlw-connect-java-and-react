db8ebf295cf68947c068e48c9e159c89ed31b69d
Matheus Florêncio <68713424+florencioMath@users.noreply.github.com>
2025-02-23 14:37:03 -0300

    feat: Criação do Controller.

---
+++ java/events/src/main/java/com/florenciomath/nlw_connect/events/controller/EventController.java
@@ -0,0 +1,43 @@
+package com.florenciomath.nlw_connect.events.controller;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.florenciomath.nlw_connect.events.model.Event;
+import com.florenciomath.nlw_connect.events.service.EventService;
+
+@RestController
+public class EventController {
+
+	@Autowired
+	private EventService service;
+	
+	@PostMapping("/events")
+	public Event addNewEvents(@RequestBody Event newEvent) {
+		return service.addNewEvent(newEvent);
+	}
+	
+	@GetMapping("/events")
+	public List<Event> getAllEvents() {
+		return service.getAllEvents();
+	}
+	
+	@GetMapping("/events/{prettyName}")
+	public ResponseEntity<Event> getEventByPrettyName(@PathVariable String prettyName) {
+		Event evt = service.getByPrettyName(prettyName);
+		
+		if (evt != null) {
+			return ResponseEntity.ok().body(evt) ;
+		}
+		
+		return ResponseEntity.notFound().build();
+	}
+	
+}
