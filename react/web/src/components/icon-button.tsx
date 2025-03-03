01e23dd90553703251b825e36b2978b4b46b6c11
Matheus Florêncio <68713424+florencioMath@users.noreply.github.com>
2025-03-03 18:04:35 -0300

    feat: Adaptado o componente para aceitar o twMerge.

--- react/web/src/components/icon-button.tsx
+++ react/web/src/components/icon-button.tsx
@@ -1,11 +1,16 @@
 import { ComponentProps } from 'react';
 
-interface IconButtonProps extends ComponentProps<'button'> {}
+import { twMerge } from 'tailwind-merge';
 
-export function IconButton(props: IconButtonProps) {
+interface ButtonProps extends ComponentProps<'button'> {}
+
+export function IconButton({ className, ...props }: ButtonProps) {
   return (
     <button
-      className="p-1.5 bg-gray-500 text-blue rounded-md cursor-pointer transition-colors duration-300 hover:bg-blue hover:text-gray-900"
+      className={twMerge(
+        'p-1.5 bg-gray-500 text-blue rounded-md cursor-pointer transition-colors duration-300 hover:bg-blue hover:text-gray-900',
+        className
+      )}
       {...props}
     />
   );
