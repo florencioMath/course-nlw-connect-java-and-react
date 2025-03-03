a543e3f9ce785a4f303a76b675e39659e965437a
Matheus Florêncio <68713424+florencioMath@users.noreply.github.com>
2025-03-03 17:20:02 -0300

    feat: Adicionado a img de background.

--- react/web/src/app/page.tsx
+++ react/web/src/app/page.tsx
@@ -1,30 +1,3 @@
-import { ArrowRightCircle, Copy, Mail } from 'lucide-react';
-
-import { Button } from '@/components/button';
-import { IconButton } from '@/components/icon-button';
-import { InputField, InputIcon, InputRoot } from '@/components/input';
-
 export default function Home() {
-  return (
-    <main>
-      <Button>
-        Enviar
-        <ArrowRightCircle />
-      </Button>
-
-      <IconButton>
-        <Copy />
-      </IconButton>
-
-      <div>
-        <InputRoot>
-          <InputIcon>
-            <Mail />
-          </InputIcon>
-
-          <InputField type="email" placeholder="Digite seu e-mail" />
-        </InputRoot>
-      </div>
-    </main>
-  );
+  return <main></main>;
 }
