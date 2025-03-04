2095fe38aa3227da2201e94d928aa664bc629c03
Matheus Florêncio <68713424+florencioMath@users.noreply.github.com>
2025-03-03 21:16:04 -0300

    feat: Usando 'use client' para o link.

--- react/web/src/app/invite/invite-link-input.tsx
+++ react/web/src/app/invite/invite-link-input.tsx
@@ -1,21 +1,28 @@
+'use client';
+
 import { Copy, Link } from 'lucide-react';
 
 import { IconButton } from '@/components/icon-button';
 import { InputRoot, InputIcon, InputField } from '@/components/input';
 
-export function InviteLinkInput() {
+interface InviteLinkInputProps {
+  inviteLink: string;
+}
+
+export function InviteLinkInput({ inviteLink }: InviteLinkInputProps) {
+  function copyInviteLink() {
+    navigator.clipboard.writeText(inviteLink);
+  }
+
   return (
     <InputRoot>
       <InputIcon>
         <Link className="size-5" />
       </InputIcon>
 
-      <InputField
-        readOnly
-        defaultValue="http://localhost:3000/invite/3291381203801293812313"
-      />
+      <InputField readOnly defaultValue={inviteLink} />
 
-      <IconButton className="-mr-2">
+      <IconButton className="-mr-2" onClick={copyInviteLink}>
         <Copy className="size-5" />
       </IconButton>
     </InputRoot>
