a31687a46faa578e3a78ab5fbe6a6289bdeb671f
Matheus Florêncio <68713424+florencioMath@users.noreply.github.com>
2025-03-06 21:15:01 -0300

    feat: Integração da posição do ranking.

--- react/web/src/app/invite/[subscriberId]/stats.tsx
+++ react/web/src/app/invite/[subscriberId]/stats.tsx
@@ -1,6 +1,7 @@
 import {
   getSubscriberInviteClicks,
   getSubscriberInviteCount,
+  getSubscriberRankingPosition,
 } from '@/http/api';
 import { BadgeCheck, Medal, MousePointerClick } from 'lucide-react';
 
@@ -11,6 +12,9 @@ interface StatsProps {
 export async function Stats({ subscriberId }: StatsProps) {
   const { count: acessCount } = await getSubscriberInviteClicks(subscriberId);
   const { count: inviteCount } = await getSubscriberInviteCount(subscriberId);
+  const { position: rankingPosition } = await getSubscriberRankingPosition(
+    subscriberId
+  );
 
   return (
     <div className="grid md:grid-cols-3 gap-3">
@@ -40,7 +44,7 @@ export async function Stats({ subscriberId }: StatsProps) {
 
       <div className="relative rounded-xl bg-gray-700 border border-gray-600 px-4 py-7 flex flex-col items-center justify-center gap-1">
         <span className="font-heading text-2xl font-semibold text-gray-200 leading-none">
-          3&ordm;
+          {rankingPosition ? `${rankingPosition}º` : '-'}
         </span>
 
         <span className="text-sm text-gray-300 leading-none text-center">
