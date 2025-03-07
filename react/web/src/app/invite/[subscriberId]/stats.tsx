00676374dcab19182acb70703a831b4ff787467d
Matheus Florêncio <68713424+florencioMath@users.noreply.github.com>
2025-03-06 21:11:13 -0300

    feat: Integração do Inscrições feitas.

--- react/web/src/app/invite/[subscriberId]/stats.tsx
+++ react/web/src/app/invite/[subscriberId]/stats.tsx
@@ -1,4 +1,7 @@
-import { getSubscriberInviteClicks } from '@/http/api';
+import {
+  getSubscriberInviteClicks,
+  getSubscriberInviteCount,
+} from '@/http/api';
 import { BadgeCheck, Medal, MousePointerClick } from 'lucide-react';
 
 interface StatsProps {
@@ -7,6 +10,7 @@ interface StatsProps {
 
 export async function Stats({ subscriberId }: StatsProps) {
   const { count: acessCount } = await getSubscriberInviteClicks(subscriberId);
+  const { count: inviteCount } = await getSubscriberInviteCount(subscriberId);
 
   return (
     <div className="grid md:grid-cols-3 gap-3">
@@ -24,7 +28,7 @@ export async function Stats({ subscriberId }: StatsProps) {
 
       <div className="relative rounded-xl bg-gray-700 border border-gray-600 px-4 py-7 flex flex-col items-center justify-center gap-1">
         <span className="font-heading text-2xl font-semibold text-gray-200 leading-none">
-          1042
+          {inviteCount}
         </span>
 
         <span className="text-sm text-gray-300 leading-none text-center">
